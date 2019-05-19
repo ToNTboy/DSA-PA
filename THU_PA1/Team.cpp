f09b1702287f8884b03aee87916180cc635d7a37
ToNTboy <ToNTboy@users.noreply.github.com>
2019-05-19 11:48:18 +0800

    Create Team.cpp

---
+++ THU_PA1/Team.cpp
@@ -0,0 +1,25 @@
+include <cstdio>
+
+int main() {
+    // accept input
+    int n, k;
+    int *A, *B, *C;
+    scanf("%d", &n);
+    A = new int[n];
+    B = new int[n];
+    C = new int[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", A[i]);
+    }
+    for (int i = 0; i < n; i++) {
+        scanf("%d", B[i]);
+    }
+    for (int i = 0; i < n; i++) {
+        scanf("%d", C[i]);
+    }
+    scanf("%d", &k);
+
+    // judge
+    
+    return 0;
+}
